89b9d00f8083432885bb83a642e621af39e22bf2
Saboteur <79015406+Saboteur0201@users.noreply.github.com>
2025-03-17 02:09:46 +0000

    Minor changes to GUI and Help class

---
+++ Help.java
@@ -0,0 +1,32 @@
+package bloodtestscheduler;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author saboteur
+ */
+
+public class Help {
+    
+    public static void showHelp() { // This makes it easy for anyone using this app to understand how it works
+        String helpMessage = "Blood Test Scheduler Help\n\n"
+                + "Adding Patients:\n"
+                + "1. Enter the patient's Name, Age, and Gender.\n"
+                + "2. Select the Priority Level (Urgent, Medium, Low).\n"
+                + "3. Check the box if they are a Hospital Patient.\n"
+                + "4. Assign a GP if needed.\n"
+                + "5. Click 'Add Patient' to add them to the queue.\n\n"
+                + "Processing Patients:\n"
+                + "- Click 'Process Next' show the highest priority/next up patient.\n"
+                + "- If a patient is not present, mark them as a No-Show (Yes).\n\n"
+                + "Additional Features:\n"
+                + "- Click 'Show GP Details' to view all doctors.\n"
+                + "- Click 'Clear Patient Queue' to remove all patients.\n"
+                + "- Click 'Quit' to close the application.\n\n\n"
+                + "That's the end of this help section!";
+
+        // Display the help message in a dialog box
+        JOptionPane.showMessageDialog(null, helpMessage, "Help", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
